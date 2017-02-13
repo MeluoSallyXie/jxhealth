a5304ab4c08dd035176d32aadf8d00c111beaa40
sallyxie <sallyxie@meluo.net>
2017-02-13 13:44:36 +0800

    调试

--- app/js/components/HomeNav.js
+++ app/js/components/HomeNav.js
@@ -7,9 +7,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HomeNav = React.createClass({
+    handleClick:function(){
+        alert("此服务暂未开放");
+    },
     render: function() {
         return (
-            <div className={this.props.className}>
+            <div className={this.props.className} onClick={this.handleClick}>
             <img src={this.props.srcimg} />
             <div>{this.props.description}</div>
             </div>
--- app/js/modules/Home.js
+++ app/js/modules/Home.js
@@ -6,17 +6,15 @@ import { Link } from 'react-router'
 import HomeNav from '../components/HomeNav'
 
 export default React.createClass({
-    handleClick:function(){
-        alert("此服务暂未开放");
-    },
+
     render:function() {
     return (
         <div>
             <Link to="/repos">
                  <HomeNav description="泌乳调理" srcimg="app/image/homenavimg1.png" className="homenav nav1" />
             </Link>
-            <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2" onClick={this.handleClick} />
-            <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3" onClick={this.handleClick} />
+            <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2"  />
+            <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3"  />
         </div>
     );
 }
