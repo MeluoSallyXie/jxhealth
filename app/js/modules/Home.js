73b21a1e0475560945750efb266fb6874ca10550
sallyxie <sallyxie@meluo.net>
2017-02-13 13:35:52 +0800

    调试

--- app/js/modules/Home.js
+++ app/js/modules/Home.js
@@ -6,14 +6,17 @@ import { Link } from 'react-router'
 import HomeNav from '../components/HomeNav'
 
 export default React.createClass({
-    render() {
+    handleClick:function(){
+        alert("此服务暂未开放");
+    },
+    render:function() {
     return (
         <div>
             <Link to="/repos">
                  <HomeNav description="泌乳调理" srcimg="app/image/homenavimg1.png" className="homenav nav1" />
             </Link>
-            <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2" />
-            <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3" />
+            <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2" onClick={this.handleClick} />
+            <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3" onClick={this.handleClick} />
         </div>
     );
 }
