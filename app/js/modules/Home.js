3a24ca8dccb024cf7334a87f178edb8a4c12c778
sallyxie <sallyxie@meluo.net>
2017-02-13 12:16:56 +0800

    调试

--- app/js/modules/Home.js
+++ app/js/modules/Home.js
@@ -6,7 +6,7 @@ import React from 'react'
 export default React.createClass({
     render() {
     return <div className="homenav nav1">
-        <img src="app/image/homenavimg1.png">
+        <img src="app/image/homenavimg1.png" />
         <div>泌乳调理</div>
         </div>
 }
