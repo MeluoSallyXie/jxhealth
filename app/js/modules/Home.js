f2a92b9ec77bd93759ea14d8a52094c8c0bf0f5c
sallyxie <sallyxie@meluo.net>
2017-02-13 12:36:41 +0800

    调试

--- app/js/modules/Home.js
+++ app/js/modules/Home.js
@@ -2,12 +2,14 @@
  * Created by sally on 2017/2/11.
  */
 import React from 'react'
+var HomeNav = require('./../components/HomeNav');
 
 export default React.createClass({
     render() {
-    return <div className="homenav nav1">
-        <img src="app/image/homenavimg1.png" />
-        <div>泌乳调理</div>
+    return (
+        <div>
+            <HomeNav />
         </div>
+    )
 }
 })
