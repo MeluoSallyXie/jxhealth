4df5e394e456e9d1fede7a85202c799848e8ae48
你çsallyxiemm <sallyxie@inception4.com>
2017-02-14 17:06:47 +0800

    调试

--- app/js/components/ProductItem.js
+++ app/js/components/ProductItem.js
@@ -9,21 +9,45 @@ var ReactDOM = require('react-dom');
 var ProductItem = React.createClass({
     render: function () {
         var item;
-        item = (
-            <div className="productItem">
-                <a href="#"><img src={this.props.thumb}/></a>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td width="50%" className="title">{this.props.name}</td>
-                        <td className="money borderRNone">
-                            ￥<label className="moneyNum">{this.props.price}</label>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        );
+        if(Number(this.props.service_timer)==0){
+            item = (
+                <div className="productItem">
+                    <a href="#"><img src={this.props.thumb}/></a>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td width="50%" className="title">{this.props.name}</td>
+                            <td className="money borderRNone">
+                                ￥<label className="moneyNum">{this.props.price}</label>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+        else {
+            item = (
+                <div className="productItem">
+                    <a href="#"><img src={this.props.thumb}/></a>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td style="width: 33%;" class="title">{this.props.name}</td>
+                            <td className="money">
+                                ￥<label className="moneyNum">{this.props.price}</label>
+                            </td>
+                            <td style="width: 33%;">
+                            <span className="time">
+                               {this.props.service_timer}分钟
+                            </span>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
         return item;
     }
 });
--- app/js/modules/ProductCategory.js
+++ app/js/modules/ProductCategory.js
@@ -8,7 +8,7 @@ export default React.createClass({
     render() {
         return (
             <div className="productlist">
-                <ProductItem thumb="app/image/temp/productlist1.png" name="产后开奶" price="499" />
+                <ProductItem thumb="app/image/temp/productlist1.png" name="产后开奶" price="499" service_timer="50" />
             </div>
         );
     }
