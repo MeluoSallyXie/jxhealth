222291980e417131c9e3a1bba52916737c359ed1
你çsallyxiemm <sallyxie@inception4.com>
2017-02-14 13:36:55 +0800

    调试

--- app/js/modules/Home.js
+++ app/js/modules/Home.js
@@ -4,18 +4,20 @@
 import React from 'react'
 import { Link } from 'react-router'
 import HomeNav from '../components/HomeNav'
+import BottomFooter from '../components/BottomFooter'
 
 export default React.createClass({
 
-    render:function() {
-    return (
-        <div>
-            <Link to="/repos">
-                 <HomeNav description="泌乳调理" srcimg="app/image/homenavimg1.png" className="homenav nav1" state="0" />
-            </Link>
-            <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2" state="1"  />
-            <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3" state="1" />
-        </div>
-    );
-}
+    render: function () {
+        return (
+            <div>
+                <Link to="/repos">
+                    <HomeNav description="泌乳调理" srcimg="app/image/homenavimg1.png" className="homenav nav1" state="0"/>
+                </Link>
+                <HomeNav description="产后恢复" srcimg="app/image/homenavimg2.png" className="homenav nav2" state="1"/>
+                <HomeNav description="营养膳食" srcimg="app/image/homenavimg3.png" className="homenav nav3" state="1"/>
+                <BottomFooter />
+            </div>
+        );
+    }
 })
