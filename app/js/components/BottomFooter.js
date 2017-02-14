92df13314e36d36941512a8aeb7149c3b22ef3a8
你çsallyxiemm <sallyxie@inception4.com>
2017-02-14 13:44:34 +0800

    调试

--- app/js/components/BottomFooter.js
+++ app/js/components/BottomFooter.js
@@ -16,27 +16,7 @@ var BottomFooter = React.createClass({
     render: function () {
         return (
             <div>
-                <div className="footer_mobile"></div>
-                <footer className="footer_mobile v2_footwrap">
-                    <table style="width: 100%;background-color: #3b3a3d;text-align: center" cellpadding="0"
-                           cellspacing="0">
-                        <tr>
-                            <td style="width: 33%;">
-                                <a href="#"><img
-                                    src="app/image/wechatfooter/footerhome_active.png"/><p
-                                    style="color: #fe8e19">服务首页</p></a>
-                            </td>
-                            <td>
-                                <a href="/index.php?route=press/all&page=1&limit=10"><img
-                                    src="app/image/wechatfooter/footerdoc.png"/><p >权威文章</p></a>
-                            </td>
-                            <td style="width: 33%;">
-                                <a href="#"><img
-                                    src="app/image/wechatfooter/footeruser.png"/><p >订单中心</p></a>
-                            </td>
-                        </tr>
-                    </table>
-                </footer>
+                11111
             </div>
         );
     }
