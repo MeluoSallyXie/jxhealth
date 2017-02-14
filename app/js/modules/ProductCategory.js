b50ba061463c33d6ac515d20a8eaf80270e5dff9
你çsallyxiemm <sallyxie@inception4.com>
2017-02-14 17:23:08 +0800

    调试

--- app/js/modules/ProductCategory.js
+++ app/js/modules/ProductCategory.js
@@ -8,14 +8,15 @@ export default React.createClass({
     render: function () {
         var data = {
             products: [
-                {thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 50},
-                {thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 50},
-                {thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 0}
+                {id: 1, thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 50},
+                {id: 2, thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 50},
+                {id: 3, thumb: "app/image/temp/productlist1.png", name: "产后开奶", price: 499, service_timer: 0}
             ]
         };
-        var productlist=data.products.map(function(product){
+        var productlist = data.products.map(function (product) {
             return (
-                <ProductItem thumb={product.thumb} name={product.name} price={product.price} service_timer={product.service_timer}/>
+                <ProductItem id={product.id} thumb={product.thumb} name={product.name} price={product.price}
+                             service_timer={product.service_timer}/>
             );
         });
         return (
