d3e7b8f48460b65a55ef937b01df6bb2262b3c4b
你çsallyxiemm <sallyxie@inception4.com>
2017-02-14 18:15:50 +0800

    调试

---
+++ app/js/components/Binding.js
@@ -0,0 +1,69 @@
+/**
+ * Created by sally on 2017/2/14.
+ */
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Binding = React.createClass({
+    render: function () {
+        return (
+            <form action="" method="post" enctype="multipart/form-data" className="form-horizontal" id="bindingform">
+                <table className="bind">
+                    <tr>
+                        <td>姓名</td>
+                        <td className="colwidth">
+                            <input type="text" name="realname" className="formcontroller" value={this.props.realname}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>手机</td>
+                        <td>
+                            <input type="text" name="telephone" id="telephone" className="formcontroller" value={this.props.telephone}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            验证码
+                        </td>
+                        <td>
+                            <table className="sendMsg">
+                                <tr>
+                                    <td>
+                                        <input type="text" name="smscode" id="verificationcode" />
+                                    </td>
+                                    <td className="sendMsgBtn" id="btnSendCode">
+                                        发送验证码
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>服务区域</td>
+                        <td>
+                            <!--<input type="text" name="address" class="formcontroller"/>-->
+                            <input id="address" name="district" className="formcontroller" type="text" readonly="" placeholder="选择区域"/>
+                            <input id="addressvalue" type="hidden"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>详细地址</td>
+                        <td>
+                            <input type="text" name="address_1" className="formcontroller" value=""/>
+                        </td>
+                    </tr>
+                </table>
+                <!--?php echo $provs_data; ?-->
+                <div className="bindBottom">
+                    <span className="whitebtn active bindBtn" id="register_submitbtn">提交</span>
+                </div>
+            </form>
+        );
+    }
+});
+module.exports = Binding;
+
+
+
--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,7 @@ render((
     <Router history={hashHistory}>
         <Route path="/" component={Home}/>
         <Route path="/productcategory/:id" component={ProductCategory}/>
+        <Route path="/binding" component={Binding}/>
         <Route path="/repos" component={Repos}/>
     </Router>
 ), document.getElementById('app'));
