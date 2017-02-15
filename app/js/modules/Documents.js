8d059e1d034cb79e056d1d1cc7d49053ebdc9b0e
你çsallyxiemm <sallyxie@inception4.com>
2017-02-15 19:50:27 +0800

    调试

--- app/js/modules/Documents.js
+++ app/js/modules/Documents.js
@@ -3,7 +3,7 @@
  */
 import React from 'react'
 import { Link } from 'react-router'
-import DocumentDetail from '../components/DocumentDetail'
+import Document from '../components/Document'
 
 export default React.createClass({
     componentDidMount:function() {
