4255fe777932bd9fd679fba94776be091bf52765
你çsallyxiemm <sallyxie@inception4.com>
2017-02-24 22:34:33 +0800

    调试

--- app/js/modules/Document.js
+++ app/js/modules/Document.js
@@ -4,24 +4,23 @@
 import React from 'react'
 import { Link } from 'react-router'
 let jsonp = require('../lib/jsonp');
-import ReactDOMServer from 'react-dom/server'
 
 export default React.createClass({
     getInitialState: function () {
         return {
             title: "",
-            answer:""
+            answer: {__html: ""}
         };
     },
     componentDidMount: function () {
         var postData = {"document_id": this.props.params.id};
         jsonp("/faq/document", postData, "POST", function (data) {
             if (data.code == 0) {
-                var answer=data.data.documents[0].answer.replaceBar();
+                var answer = data.data.documents[0].answer.replaceBar();
                 console.log(answer);
                 this.setState({
                     title: data.data.documents[0].title,
-                    answer: answer,
+                    answer: {__html: data.data.documents[0].answer},
                 });
             }
             else {
@@ -36,7 +35,7 @@ export default React.createClass({
                     <label>{this.state.title}</label>
                 </div>
                 <div className="document_answer">
-                    <p>{ReactDOMServer.renderToString(this.state.answer)}</p>
+                    <p dangerouslySetInnerHTML={this.state.answer}></p>
                 </div>
             </div>
         );
