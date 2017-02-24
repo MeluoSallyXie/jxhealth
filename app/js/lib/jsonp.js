82a7039edc9648ec0b3ee271334e20e79ade046a
你çsallyxiemm <sallyxie@inception4.com>
2017-02-24 13:55:58 +0800

    调试

--- app/js/lib/jsonp.js
+++ app/js/lib/jsonp.js
@@ -8,35 +8,35 @@
 ;(function () {
     var jsonp = function (url, data, method, callback) {
         var http = require('http');
-         var querystring = require('querystring');
-         var postData = data;
-         var options = {
-         hostname: 'test.jinxingjk.com',
-         port: 80,
-         path: url,
-         method: method,
-         header: {
-         //'Content-Type':'application/x-www-form-urlencoded',
-         'Content-Type': 'application/x-www-form-urlencoded',
-         'Content-Length': Buffer.byteLength(querystring.stringify(postData))
-         }
-         }
-         var req = http.request(options, function (res) {
-         console.log('Status:', res.statusCode);
-         console.log('headers:', JSON.stringify(res.headers));
-         res.setEncoding('utf-8');
-         res.on('data', function (chun) {
-         callback(chun);
-         });
-         res.on('end', function () {
-         console.log('No more data in response.********');
-         });
-         });
-         req.on('error', function (err) {
-         console.error(err);
-         });
-         req.write(JSON.stringify(postData));
-         req.end();
+        var querystring = require('querystring');
+        var postData = data;
+        var options = {
+            hostname: 'test.jinxingjk.com',
+            port: 80,
+            path: url,
+            method: method,
+            header: {
+                //'Content-Type':'application/x-www-form-urlencoded',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(querystring.stringify(postData))
+            }
+        }
+        var req = http.request(options, function (res) {
+            console.log('Status:', res.statusCode);
+            console.log('headers:', JSON.stringify(res.headers));
+            res.setEncoding('utf-8');
+            res.on('data', function (chun) {
+                callback(chun);
+            });
+            res.on('end', function () {
+                console.log('No more data in response.********');
+            });
+        });
+        req.on('error', function (err) {
+            console.error(err);
+        });
+        req.write(JSON.stringify(postData));
+        req.end();
     };
     window.jsonp = jsonp;
 })();
