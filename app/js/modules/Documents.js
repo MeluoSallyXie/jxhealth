654fb88c0881140755c189b4a62564d96b3ea163
你çsallyxiemm <sallyxie@inception4.com>
2017-02-24 13:42:03 +0800

    调试

--- app/js/modules/Documents.js
+++ app/js/modules/Documents.js
@@ -4,25 +4,43 @@
 import React from 'react'
 import { Link } from 'react-router'
 import DocumentDetail from '../components/DocumentDetail'
+let jsonp = require('../lib/jsonp');
 
 export default React.createClass({
-    componentDidMount:function() {
-        document.body.style.backgroundColor="#eee";
+    getInitialState: function () {
+        return {
+            documents: null
+        };
     },
-    componentWillUnmount:function() {
-        document.body.style.backgroundColor="white";
+    componentDidMount: function () {
+        document.body.style.backgroundColor = "#eee";
+        var postData = null;
+        jsonp("/faq/documents", postData, "POST", function (data) {
+            if (data.code == 0) {
+                this.setState({
+                    documents: data.data.documents
+                });
+            }
+            else {
+                alert(data.message)
+            }
+        });
+    },
+    componentWillUnmount: function () {
+        document.body.style.backgroundColor = "white";
     },
     render: function () {
         var data = {
             documents: [
-                {id: 1, title:"测试",action:"/documents/1"},
-                {id: 2, title:"测试",action:"/documents/1"},
-                {id: 3, title:"测试",action:"/documents/1"}
+                {id: 1, title: "测试", action: "/documents/1"},
+                {id: 2, title: "测试", action: "/documents/1"},
+                {id: 3, title: "测试", action: "/documents/1"}
             ]
         };
         var documentlist = data.documents.map(function (document) {
             return (
-                <Link to={document.action} ><DocumentDetail key={document.id} title={document.title} action={document.action}/></Link>
+                <Link to={document.action}><DocumentDetail key={document.id} title={document.title}
+                                                           action={document.action}/></Link>
             );
         });
         return (
--- app/js/modules/PressAll.js
+++ app/js/modules/PressAll.js
@@ -5,19 +5,38 @@ import React from 'react'
 import { Link } from 'react-router'
 import BlogItem from '../components/BlogItem'
 import BottomFooter from '../components/BottomFooter'
+let jsonp = require('../lib/jsonp');
 
 export default React.createClass({
+    getInitialState: function () {
+        return {
+            presses: null
+        };
+    },
+    componentDidMount: function () {
+        var postData = null;
+        jsonp("/faq/documents", postData, "POST", function (data) {
+            if (data.code == 0) {
+                this.setState({
+                    presses: data.data.documents
+                });
+            }
+            else {
+                alert(data.message)
+            }
+        });
+    },
     render: function () {
-        var data = {
+        /*var data = {
             presses: [
                 {id: 1, title: "测试", action: "/pressall/1", thumb: ""},
                 {id: 2, title: "测试", action: "/pressall/1", thumb: ""},
                 {id: 3, title: "测试", action: "/pressall/1", thumb: ""}
             ]
-        };
-        var bloglist = data.presses.map(function (press) {
+        };*/
+        var bloglist = data.state.presses.map(function (press) {
             return (
-                <BlogItem key={press.id} title={press.title} action={press.action}/>
+                <BlogItem key={press.faq_id} title={press.title} action={"/pressall/"+press.faq_id}/>
             );
         });
         return (
