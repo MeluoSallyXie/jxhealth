2433b72d0ebb951f62de4475c95751f88804f018
你çsallyxiemm <sallyxie@inception4.com>
2017-02-24 22:18:30 +0800

    调试

--- app/js/modules/Document.js
+++ app/js/modules/Document.js
@@ -16,10 +16,11 @@ export default React.createClass({
         var postData = {"document_id": this.props.params.id};
         jsonp("/faq/document", postData, "POST", function (data) {
             if (data.code == 0) {
-                console.log(data.data.documents[0].answer.replaceBar());
+                var answer=data.data.documents[0].answer.replaceBar();
+                console.log(answer);
                 this.setState({
                     title: data.data.documents[0].title,
-                    answer: data.data.documents[0].answer.replaceBar(),
+                    answer: answer,
                 });
             }
             else {
