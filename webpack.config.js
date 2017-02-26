fbb93c0d74d12f6ea2226d5e60d698daac9a5145
你çsallyxiemm <sallyxie@inception4.com>
2017-02-26 13:52:03 +0800

    调试

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-  entry: './app/js/main.js',
+    entry: './app/js/main.js',
 
-  output: {
-    filename: 'bundle.js',
-    publicPath: ''
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
-      {test: /\.css$/, loader: 'style-loader!css-loader'}
-    ]
-  },
-  devServer: {
-    hot: true,
-    inline: true
-  }
+    output: {
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react'},
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        inline: true
+    }
 }
