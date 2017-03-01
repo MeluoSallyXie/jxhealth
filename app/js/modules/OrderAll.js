79fb803894e605bcbfeacd7e52ab6eb2ce878132
你çsallyxiemm <sallyxie@inception4.com>
2017-03-01 13:42:38 +0800

    调试

--- app/js/modules/OrderAll.js
+++ app/js/modules/OrderAll.js
@@ -1,7 +1,4 @@
 /**
- * Created by sally on 2017/2/27.
- */
-/**
  * Created by sally on 2017/2/14.
  */
 import React from 'react'
@@ -22,14 +19,12 @@ export default React.createClass({
             if (data.code == 0) {
                 var orderlist = data.data.orders.map(function (order, index) {
                     return (
-                        <div>
-                            <Link to="/repos" key={index}>
-                                <OrderItem order_id={order.products} shipping_city={order.shipping_city}
-                                           shipping_address_1={order.shipping_address_1}
-                                           shipping_date={order.shipping_date} totals={order.totals[2].text}
-                                           products={order.products}/>
-                            </Link>
-                        </div>
+                        <Link to="/repos" key={index}>
+                            <OrderItem order_id={order.products} shipping_city={order.shipping_city}
+                                       shipping_address_1={order.shipping_address_1}
+                                       shipping_date={order.shipping_date} totals={order.totals[2].text}
+                                       products={order.products}/>
+                        </Link>
                     );
                 });
                 this.setState({
