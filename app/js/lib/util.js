88b494db016e3138164ca6bda1e4ecf2d2d31e01
你çsallyxiemm <sallyxie@inception4.com>
2017-03-02 20:22:49 +0800

    调试

--- app/js/lib/util.js
+++ app/js/lib/util.js
@@ -5,7 +5,7 @@ String.prototype.trim=function(){
     return this.replace(/(^\s*)|(\s*$)/g, "");
 }
 
-window.hasClass=function(obj, cls){
+window.hasClass=function(elem, cls){
     cls = cls || '';
     if (cls.replace(/\s/g, '').length == 0) return false; //当cls没有参数时，返回false
     return new RegExp(' ' + cls + ' ').test(' ' + elem.className + ' ');
