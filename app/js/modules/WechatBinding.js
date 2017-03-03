47705bbc76e694a135becd2a97cfb958ee881eda
你çsallyxiemm <sallyxie@inception4.com>
2017-03-03 10:20:10 +0800

    调试

--- app/js/modules/WechatBinding.js
+++ app/js/modules/WechatBinding.js
@@ -2,7 +2,7 @@
  * Created by sally on 2017/2/14.
  */
 import React from 'react'
-import { Link } from 'react-router'
+import { Router,RouterContext } from 'react-router'
 require("../lib/util");
 let jsonp = require('../lib/jsonp');
 
