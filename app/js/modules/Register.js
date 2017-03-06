92c644a9cf26cbc230d9afbf3d2c95e513c151a2
你çsallyxiemm <sallyxie@inception4.com>
2017-03-06 21:27:14 +0800

    调试

--- app/js/modules/Register.js
+++ app/js/modules/Register.js
@@ -21,7 +21,7 @@ export default React.createClass({
             telephone: "",
             barcode: "",
             birthday: "",
-            department: "",
+            department: "111",
             departmentvalue: "",
             height: "",
             lastmenstrualdate: "",
